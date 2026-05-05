96079b3bdbbb854b309be70280e07475ae73a26b
Peter van de Pas <peter.vdpas@protonmail.com>
2026-05-05 10:06:47 +0200

    removed ui templates for ssr

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"embed"
 	"log"
 	"os"
 
 	"github.com/petervdpas/formidable2/internal/app"
 	"github.com/petervdpas/formidable2/internal/modules/journal"
-	"github.com/petervdpas/formidable2/internal/ui/handlers"
 	"github.com/wailsapp/wails/v3/pkg/application"
 )
 
-const appVersion = "0.1.0-dev"
+//go:embed all:frontend/dist
+var assets embed.FS
 
 func init() {
 	// Register journal events with the Wails binding generator so the
@@ -26,27 +27,18 @@ func main() {
 		log.Fatal(err)
 	}
 
-	uiHandler := handlers.New(handlers.Deps{
-		Template: a.Template,
-		Storage:  a.Storage,
-		Config:   a.Config,
-		AppName:  "Formidable2",
-		Version:  appVersion,
-	})
-
 	wapp := application.New(application.Options{
 		Name:        "Formidable2",
 		Description: "Editor for templates and Markdown forms",
 		Services:    a.WailsServices(),
 		Assets: application.AssetOptions{
-			Handler: uiHandler,
+			Handler: application.AssetFileServerFS(assets),
 		},
 		Mac: application.MacOptions{
 			ApplicationShouldTerminateAfterLastWindowClosed: true,
 		},
 	})
 
-	// Bridge journal events into Wails events.
 	a.SetEmit(func(name string, data any) {
 		wapp.Event.Emit(name, data)
 	})
